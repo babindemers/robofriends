01479da1f55072ed8f46eb3e88b118edb0f9098d
babindemers <babw2501@usherbrooke.ca>
2020-05-08 20:35:53 -0400

    Deploy

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Scroll(props) {
     return (
-        <div style={{overflowY: 'scroll', borderTop: '1px solid black', height: '500px'}}>
+        <div style={{overflowY: 'scroll', border: '1px solid black' , height: '700px'}}>
             {props.children}
         </div>
     );
